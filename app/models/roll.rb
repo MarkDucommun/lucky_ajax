efaf4bba41b87f47670011f206afd715cb774330
Apprentice <apprentice@dbc-chicago-009.local>
2013-08-07 08:51:36 -0500

    writing javascript

--- app/models/roll.rb
+++ app/models/roll.rb
@@ -7,6 +7,7 @@ class Roll < ActiveRecord::Base
 
   after_initialize :roll_if_value_is_nil
 
+
   private
   # If the user passes-in a "value", let's use it. Otherwise, we'll generate a random one.
   def roll_if_value_is_nil
--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,16 @@
+
 $(document).ready(function () {
 
   // PSEUDO-CODE:
   //   1- intercept the form submission event using jQuery
+  $("#die").click(function(event){
   //   2- prevent the default action for that event from happening
+    event.preventDefault();
   //   3- generate a random number between 1 and 6 using JavaScript
+    var roll_data = Math.floor((Math.random() * 6) + 1);
   //   4- use jQuery to submit an AJAX post to the form's action
+
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
 
+  });
 });
